b16de8490798a81970296f36391a8f1992342e1a
Joe Jackson <joe.jackson@parall.ax>
2020-02-13 19:28:44 +0000

    god knows

---
+++ client/build/precache-manifest.8b4bd96a95dac69568cd68462fb857e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c65fcdd49a7fd02b3e7e3497c7b2f53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c499ecb3a71f782acc4",
+    "url": "/static/css/main.4308ca45.chunk.css"
+  },
+  {
+    "revision": "c35b6ec0a45c0dec2ad3",
+    "url": "/static/js/2.06d83938.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.06d83938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c499ecb3a71f782acc4",
+    "url": "/static/js/main.2b888802.chunk.js"
+  },
+  {
+    "revision": "c77bbcc758cce5db94b7",
+    "url": "/static/js/runtime-main.61a4824d.js"
+  }
+]);
