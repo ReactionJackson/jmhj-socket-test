639c5b834c94eea477e717634f3666a6f5860470
Joe Jackson <joe.jackson@parall.ax>
2020-02-13 16:24:59 +0000

    restructure

---
+++ client/build/precache-manifest.1d520c682ee2ece8ce56251603a95801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b293edef6385332d7c4cc08fda72ab68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b275f2f609a6d69f2e4e",
+    "url": "/static/css/main.4308ca45.chunk.css"
+  },
+  {
+    "revision": "c35b6ec0a45c0dec2ad3",
+    "url": "/static/js/2.06d83938.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.06d83938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b275f2f609a6d69f2e4e",
+    "url": "/static/js/main.8fbac1c5.chunk.js"
+  },
+  {
+    "revision": "c77bbcc758cce5db94b7",
+    "url": "/static/js/runtime-main.61a4824d.js"
+  }
+]);
